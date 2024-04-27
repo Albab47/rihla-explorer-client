6431070cd95c931d84c3ef474bca71a855ce2074
Albab47 <115328782+Albab47@users.noreply.github.com>
2024-04-27 14:04:53 +0600

    Setup AuthProvider and Authcontext

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,13 @@
+import { useContext } from "react";
+import { AuthContext } from "../providers/AuthProvider";
 
 const HomePage = () => {
+    const {msg} = useContext(AuthContext)
+    
     return (
         <>
             <h2 className='text-xl text-center text-amber-600 mt-12 container mx-auto'>Home Page coming soon...</h2>
+            <p>{msg}</p>
         </>
     );
 };
---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,26 @@
+import PropTypes from "prop-types"; 
+import { getAuth } from "firebase/auth";
+import app from "../firebase/firebase.config";
+import { createContext } from "react";
+
+// Firebase Auth
+const auth = getAuth(app);
+
+export const AuthContext = createContext();
+
+const AuthProvider = ({children}) => {
+
+    const authInfo = {msg: 'Auth info coming..'}
+    
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+AuthProvider.propTypes = {
+  children: PropTypes.node,
+};
+
+export default AuthProvider;
