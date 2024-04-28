d64253c77bbdc8af7e34863c5b7807ba450a85fb
Albab47 <115328782+Albab47@users.noreply.github.com>
2024-04-28 22:41:31 +0600

    Create custom hook to fetch spots data

---
+++ src/hooks/useSpotsData.js
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+const useSpotsData = () => {
+  const [spots, setSpots] = useState();
+  const [toggle, setToggle] = useState(false);
+
+  const refetch = () => {
+    setToggle(!toggle);
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("http://localhost:5000/spots");
+      const data = res.json()
+      setSpots(data)
+    };
+    fetchData()
+  }, [toggle]);
+
+  return {spots, refetch};
+};
+
+export default useSpotsData;
