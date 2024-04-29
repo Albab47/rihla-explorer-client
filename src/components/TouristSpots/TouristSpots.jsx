6b01f8b64aed89388c3c9b61dd3d9d01c11b8227
Albab47 <115328782+Albab47@users.noreply.github.com>
2024-04-29 09:01:20 +0600

    Get data from server and show to UI

--- src/components/TouristSpots/TouristSpots.jsx
+++ src/components/TouristSpots/TouristSpots.jsx
@@ -1,25 +1,27 @@
 import useSpotsData from "../../hooks/useSpotsData";
 import SpotCard from "../SpotCard/SpotCard";
 
-const TouristSpots = () => {
-  const { spots } = useSpotsData();
-  console.log(spots);
+const TouristSpots = ({isHome = false}) => {
+  const { spots: loadedSpots } = useSpotsData();
+  const spots = isHome ? loadedSpots.slice(0, 6) : loadedSpots;
 
   return (
     <section className="my-24">
       <div className="max-w-screen-xl mx-auto px-5">
         {/* heading */}
         <header className="text-center max-w-xl mx-auto space-y-5">
-          <h1 className="text-3xl md:text-5xl font-display ">Tourists Spots</h1>
-          <p className="text-sm sm:text-base text-light">
-            Embark on a journey through the Middle East's iconic tourist spots.
-            Explore ancient wonders, breathtaking landscapes in one
-            unforgettable adventure.
-          </p>
+          <h1 className="text-3xl md:text-5xl font-display ">{isHome ? "Popular" : "Browse"} Tourists Spots</h1>
+         {isHome && (
+           <p className="text-sm sm:text-base text-light">
+           Embark on a journey through the Middle East's iconic tourist spots.
+           Explore ancient wonders, breathtaking landscapes in one
+           unforgettable adventure.
+         </p>
+         )}
         </header>
 
         {/* Spots card */}
-        <div className="grid gap-5 sm:grid-cols-2 md:grid-cols-3">
+        <div className="grid gap-5 sm:grid-cols-2 md:grid-cols-3 mt-16 justify-center">
           {spots?.map((spot) => (
             <SpotCard key={spot._id} spot={spot} />
           ))}
@@ -28,5 +30,12 @@ const TouristSpots = () => {
     </section>
   );
 };
+
+import PropTypes from "prop-types"; 
+
+
+TouristSpots.propTypes = {
+  isHome: PropTypes.bool,
+}
 
 export default TouristSpots;
